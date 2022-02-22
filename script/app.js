aed438e5bc894ad7905016bfad73b25a2030bcaf
Marco <mar.manunta@gmail.com>
2022-02-22 10:15:37 +0100

    got the squares to be selected properly

--- script/app.js
+++ script/app.js
@@ -1,26 +1,150 @@
-/***
- *
- *
- * FOR LATER: ANIMATE BOARDS
- *
- *
- */
-
 /**
- *  UI
+ * TODOS
+ *  - CHECK IF SLOT IS ALREADY TAKEN
+ *  - GENERALIZE CHECKIFWIN FUNCTION TO USE IT ALSO WITH MEGABOARD
+ *  - TURN BOARD INTO A CLASS AND MEGABOARD INTO A CLASS THAT EXTENDS BOARD
+ *  - REMOVE EVENTLISTENERS FROM UNPLAYABLE BOARDS
  */
 
-// DECLARE MAIN ARRAY AND SET IT TO []
-const boardEl = document.querySelector('#board');
+// DOM SHIT
+const h1 = document.querySelector('h1');
+const megaBoardEl = document.querySelector('#mega-board');
 const slotsDivs = [];
-const slotsCount = 9;
-for (let i = 0; i < slotsCount; i++) {
-  const slot = document.createElement('div');
-  slot.classList.add('slot');
-  boardEl.appendChild(slot);
+const boardCount = 9;
+
+for (let i = 0; i < boardCount; i++) {
+  const boardEl = document.createElement('div');
+  boardEl.setAttribute('data-index', i);
+  boardEl.classList.add('board');
+  megaBoardEl.appendChild(boardEl);
 }
 
-// FOR EVERY BOARD MAKE AN ARRAY OF 9 DIVS AND PUSH IT INTO MAIN ARRAY
+const domBoards = document.querySelectorAll('.board');
+
+domBoards.forEach(board => {
+  for (let i = 0; i < boardCount; i++) {
+    const slot = document.createElement('div');
+    slot.classList.add('slot');
+    board.appendChild(slot);
+  }
+});
+
+// GAME STATE
+class Player {
+  constructor(name, marker) {
+    this.name = name;
+    this.marker = marker;
+    this.wins = [];
+  }
+}
+
+// const logicBoards = new Array(9)
+//   .fill({
+//     'Player 1': {
+//       choices: [],
+//     },
+//     'Player 2': {
+//       choices: [],
+//     },
+//     isPlayable: true,
+//   })
+//   .map((board, i) => ({ ...board, id: i }));
+
+// console.log(logicBoards);
+
+const logicBoards = [
+  {
+    id: 0,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 1,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 2,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 3,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 4,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 5,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 6,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 7,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+  {
+    id: 8,
+    'Player 1': {
+      choices: [],
+    },
+    'Player 2': {
+      choices: [],
+    },
+    isPlayable: true,
+  },
+];
+
 const winningPossibilities = [
   [0, 1, 2],
   [3, 4, 5],
@@ -32,37 +156,78 @@ const winningPossibilities = [
   [2, 4, 6],
 ];
 
-const board = {
-  playerChoices: [0, 1, 2],
-  isPlayable: true,
+// GAME LOGIC
+const player1 = new Player('Player 1', 'X');
+const player2 = new Player('Player 2', 'O');
+
+let player = player1;
+let playBoard = null;
+
+domBoards.forEach((board, index) => {
+  [...board.children].forEach((slot, i) => {
+    slot.addEventListener('click', () => {
+      playBoard = logicBoards[index];
+      console.log(playBoard);
+      playBoard[player.name].choices.push(i);
+      slot.classList.add('taken');
+      slot.textContent = player.marker;
+      if (checkIfWin(playBoard, player)) {
+        h1.textContent = `${player.name} wins`;
+        player.wins.push(index);
+        console.log(playBoard);
+        playBoard.isPlayable = false;
+      } else if (isFullBoard(board)) {
+        playBoard.isPlayable = false;
+      } else {
+        nextBoard(index, i);
+      }
+
+      player = player === player1 ? player2 : player1;
+    });
+  });
+});
+
+const checkIfWin = (board, player) =>
+  winningPossibilities.filter(possibility =>
+    possibility.every(index => board[player.name].choices.includes(index))
+  ).length > 0;
+
+// UTILITY FUNCTIONS
+const isFullBoard = board =>
+  [...board.children].every(slot => slot.classList.contains('taken'));
+
+const playableBoards = [...domBoards].filter((board, i) => {
+  if (logicBoards[i].isPlayable) {
+    return board;
+  }
+});
+
+console.log(playableBoards);
+
+const allPlayable = boardsArray =>
+  boardsArray.forEach((board, i) => {
+    board.isPlayable = true;
+    domBoards[i].classList.remove('non-playable');
+  });
+
+const nextBoard = (prevBoard, index) => {
+  if (prevBoard === index) {
+    allPlayable(playableBoards);
+    domBoards[index].classList.add('non-playable');
+  } else {
+    domBoards.forEach((board, i) => {
+      if (i !== index) {
+        board.classList.add('non-playable');
+      }
+    });
+    if (!logicBoards[index].isPlayable || isFullBoard(domBoards[index])) {
+      allPlayable(playableBoards);
+    } else {
+      playBoard = logicBoards[index];
+      domBoards[index].classList.remove('non-playable');
+    }
+  }
+  // console.log(playableBoards);
 };
 
-const checkIfWin = board => {
-  const win = winningPossibilities.filter(possibility =>
-    possibility.every(index => board.playerChoices.includes(index))
-  );
-
-  return win.length > 0;
-};
-
-console.log(checkIfWin(board));
-
-// APPLY CLASSES FOR STYLING AND IDS BASED ON THE MAIN ARRAY INDEXES TO IDENTIFY THEM
-
-/**
- * LOGIC
- */
-
-// MAKE AN ARRAY OF ARRAYS WITH ALL POSSIBLE WINNING COMBINATION
-// SET AN OBJECT FOR EVERY BOARD WITH 2 ARRAYS THAT WILL STORE PLAYER 1 AND PLAYER TWO CHOICES
-// RUN CHECK FOR WIN FUNCTION
-// AFTER EVERY MOVE CHECK IF PLAYER HAS WON RUNNING EVERY() LOOPING OVER WINNING COMBINATIONS AND CHECKING THE PLAYER'S ARRAY
-
-// IF NO WIN:
-// PLAYER CHOOSES SLOT BY CLICKING ON IT
-// IF SLOT IS EMPTY
-// SLOT GETS ASSIGNED THE CLASS OF TAKEN AND THE CLASS OF THE PLAYER, SAY 'X' AND 'O' THE SLOT INDEX GETS PUSHED INTO THE CORRISPONDING BOARD PLAYERS ARRAY
-
-// NEXT PLAYER THEN PLAYS ON THE BOARD WITH THE INDEX CORRISPONDING TO THE INDEX OF SLOT OF FORMER PLAYER'S CHOICE
-
-// IF BOARD IS FULL (OR NON PLAYABLE, BUT THAT'S FOR ANOTHER DAY...) THE NEXT BOARD WILL BE RANDOMLY CHOSEN
+allPlayable(playableBoards);
